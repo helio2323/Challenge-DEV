25b3630355d6a0a0b856bd3c585710f7502d7f8f
Helio Fernandes dos Reis Lima <helio_lima@outlook.com.br>
2024-07-20 18:07:26 -0300

    page-ranking
    Criacao da pagina de ranking

---
+++ frontend/components/Ranking/Diario/Table.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+const people = [{
+  id: 1,
+  usuario: 'Lindsay Walton',
+  desafios: '50',
+  xp: '5500',  
+}, {
+  id: 2,
+  usuario: 'Courtney Henry',
+  desafios: '50',
+  xp: '5500',
+}, 
+]
+
+const page = ref(1)
+const pageCount = 8
+
+const rows = computed(() => {
+  return people.slice((page.value - 1) * pageCount, (page.value) * pageCount)
+})
+</script>
+
+<template>
+  <div>
+    <UTable :rows="rows" />
+    <div class="flex w-full justify-end px-3 py-3.5 border-t border-gray-200 dark:border-gray-700">
+      <UPagination v-model="page" :page-count="pageCount" :total="people.length" />
+    </div>
+  </div>
+</template>
+
+<style scoped> 
+div {
+    width: 100%;
+    align-items: end;
+    justify-content: end;
+}
+
+table {
+    width: 100%;
+}
+</style>
+
